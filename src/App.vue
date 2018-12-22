8d2b2c3b6746c941af94211302b767b5b7c03fbc
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2018-12-22 13:43:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div id="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- use hyphenated names! --> <child :my-message="msg"></child>
+</div>
+</template>
+
+<script>
+import Child from './components/Child.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Child
+  },
+  // https://vuejs.org/2016/02/06/common-gotchas/#Why-does-data-need-to-be-a-function
+  data: () => ({
+    msg: 'Hello Vue.js!'
+  }),
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Visualising a Message:</p>
+    <span>{{myMessage}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+      props: {
+         'myMessage': {
+           type: String,
+           required: true
+          }
+      }
+  }
+</script>
